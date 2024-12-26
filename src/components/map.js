b8cbcb57ae70d51371d8c30a81bd238f670e8d7a
Suzan-Kamama <kamamasuzan961@gmail.com>
2024-12-26 15:19:58 +0300

    Update map.js

--- src/components/map.js
+++ src/components/map.js
@@ -5,11 +5,11 @@ export function initMap() {
   mapScript.defer = true;
   mapScript.addEventListener('load', () => {
     const map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: -1.1022, lng: 37.0128 }, // Coordinates for Juja
+      center: { lat: -1.1022, lng: 37.0128 }, // Coordinates for Traqo Lounge
       zoom: 15,
     });
     new google.maps.Marker({
-      position: { lat: -1.1022, lng: 37.0128 },
+      position: { lat: -1.1261, lng: 37.0036 },
       map,
       title: 'Juja Mabati Factory'
     });
