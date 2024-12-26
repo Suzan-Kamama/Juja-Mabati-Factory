089e4652052d8a1ee226cc2e5f29fe37acee6fc3
Suzan-Kamama <kamamasuzan961@gmail.com>
2024-12-26 12:06:37 +0300

    Update constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -6,7 +6,7 @@ export const IMAGES = {
     'https://images.unsplash.com/photo-1577451581538-67043aba1116?auto=format&fit=crop&w=1920&q=80'
   ],
   PRODUCTS: {
-    BOX_PROFILE: 'https://github.com/Suzan-Kamama/Juja-Mabati-Factory/commit/62f2aa67b230f7f0ec2abf7f1b7e6292c3f492cb#diff-c52ddef0e64030ff2141cc484cda5a57d3763b92306b5ab3a5a366e883e14b84',
+    BOX_PROFILE: 'https://www.pinterest.com/pin/374361787781362476',
     CORRUGATED: 'https://images.unsplash.com/photo-1635424710928-0544e8512eae?auto=format&fit=crop&w=800&q=80',
     TILE: 'https://images.unsplash.com/photo-1632759145351-1d592919f522?auto=format&fit=crop&w=800&q=80'
   }
