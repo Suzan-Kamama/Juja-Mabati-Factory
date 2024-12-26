1c4e8a079ac79aae0c694b8afff8090809bb8250
Suzan-Kamama <kamamasuzan961@gmail.com>
2024-12-26 13:18:33 +0300

    Update constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -6,7 +6,7 @@ export const IMAGES = {
     'https://images.unsplash.com/photo-1577451581538-67043aba1116?auto=format&fit=crop&w=1920&q=80'
   ],
   PRODUCTS: {
-    BOX_PROFILE: 'https://www.pinterest.com/pin/374361787781362476?auto=format&fit=crop&w=1920&q=80',
+    BOX_PROFILE: 'src/components/Box Profile Roofing Sheets.jpeg',
     CORRUGATED: 'https://images.unsplash.com/photo-1635424710928-0544e8512eae?auto=format&fit=crop&w=800&q=80',
     TILE: 'https://images.unsplash.com/photo-1632759145351-1d592919f522?auto=format&fit=crop&w=800&q=80'
   }
