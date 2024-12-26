35777085826dbf733f3f62d450fb23c4373de4a0
Suzan-Kamama <kamamasuzan961@gmail.com>
2024-12-26 13:51:35 +0300

    Update constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -6,7 +6,7 @@ export const IMAGES = {
     'https://images.unsplash.com/photo-1577451581538-67043aba1116?auto=format&fit=crop&w=1920&q=80'
   ],
   PRODUCTS: {
-    BOX_PROFILE: 'https://raw.githubusercontent.com/Suzan-Kamama/Juja-Mabati-Factory/refs/heads/main/src/components/Box%20Profile%20Roofing%20Sheets.jpeg?token=GHSAT0AAAAAAC4FPIYDR4QHL5VB4JXHUNESZ3NGFMQ',
+    BOX_PROFILE: 'https://github.com/Suzan-Kamama/Juja-Mabati-Factory/blob/main/src/components/Box%20Profile%20Roofing%20Sheets.jpeg?raw=true',
     CORRUGATED: 'https://images.unsplash.com/photo-1635424710928-0544e8512eae?auto=format&fit=crop&w=800&q=80',
     TILE: 'https://images.unsplash.com/photo-1632759145351-1d592919f522?auto=format&fit=crop&w=800&q=80'
   }
