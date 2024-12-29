73e60c3de28a161113ac051ac2d5ffd70f127d81
Suzan-Kamama <kamamasuzan961@gmail.com>
2024-12-29 16:54:37 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@
 export default {
   content: [
     "./index.html",
+    "./page2.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./styles.css",
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+      container: {
+        center: true, // Center the container by default
+        padding: "2rem", // Add default padding
+      },
+    },
   plugins: [],
 }
