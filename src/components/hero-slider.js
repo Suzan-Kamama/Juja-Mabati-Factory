167efe63d7a5c07f30fc34427e073fad8d980a33
Suzan-Kamama <kamamasuzan961@gmail.com>
2024-12-29 22:24:42 +0300

    Update hero-slider.js

--- src/components/hero-slider.js
+++ src/components/hero-slider.js
@@ -1,19 +1,1 @@
-//import Swiper from 'swiper';
-//import { Pagination, Autoplay } from 'swiper/modules';
-//import 'swiper/css';
-//import 'swiper/css/pagination';
 
-//export function initHeroSlider() {
-  new Swiper('.hero-slider', {
-    modules: [Pagination, Autoplay],
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    loop: true
-  });
-}
