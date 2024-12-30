22fcda7f35d494e2fcb1b1be0964aad6c7e6adfe
Suzan-Kamama <kamamasuzan961@gmail.com>
2024-12-30 13:18:11 +0300

    Update constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,9 +1,9 @@
 // Image URLs for the website
 export const IMAGES = {
   HERO: [
-    'https://images.unsplash.com/photo-1577451581377-523b0a03bb6b?auto=format&fit=crop&w=708&q=80',
-    'https://images.unsplash.com/photo-1577451581039-032debfb47b6?auto=format&fit=crop&w=708&q=80',
-    'https://images.unsplash.com/photo-1577451581538-67043aba1116?auto=format&fit=crop&w=708&q=80'
+    'https://images.unsplash.com/photo-1577451581377-523b0a03bb6b?auto=format&fit=crop&w=236&q=80',
+    'https://images.unsplash.com/photo-1577451581039-032debfb47b6?auto=format&fit=crop&w=236&q=80',
+    'https://images.unsplash.com/photo-1577451581538-67043aba1116?auto=format&fit=crop&w=236&q=80'
   ],
   PRODUCTS: {
     BOX_PROFILE: 'https://github.com/Suzan-Kamama/Juja-Mabati-Factory/blob/main/src/components/Box%20Profile%20Roofing%20Sheets.jpeg?raw=true',
